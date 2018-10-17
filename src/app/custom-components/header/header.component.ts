dee00927352e2541fc6e3424928af7e9f014cd49
Mauricio Arce <mauricio.arce.mat@gmail.com>
2018-10-17 17:00:06 -0400

    Add void to on init method

--- src/app/custom-components/header/header.component.ts
+++ src/app/custom-components/header/header.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 declare const $: any;
+
 @Component({
   selector: 'sw-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  ngOnInit() {
+  ngOnInit(): void {
     $(() => {
       $('.sidenav').sidenav();
     });
