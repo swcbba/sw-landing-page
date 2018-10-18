474b768d7a90760df95c78bfbc6e19384158eaac
MariaG Garcia Flores <MariaG Garcia@jalasoft.local>
2018-10-18 15:31:56 -0400

    profiles component creates

---
+++ src/app/custom-components/profiles/profiles.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sw-profiles',
+  templateUrl: './profiles.component.html',
+  styleUrls: ['./profiles.component.scss']
+})
+export class ProfilesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
