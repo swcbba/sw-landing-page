fc4539c4d565e7f770a0f35998020a7820f9d758
Mauricio Arce <Mauricio.Arce@communitybrands.com>
2018-10-24 14:29:18 -0400

    Change to language button to capital letters

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,4 +1,4 @@
 export const LANGUAGE_KEY = 'language';
 export const ES_KEY = 'es';
 export const EN_KEY = 'en';
-export const MAP_KEY = { 'es': 'Es', 'en': 'En'};
+export const MAP_KEY = { 'es': 'ES', 'en': 'EN'};
